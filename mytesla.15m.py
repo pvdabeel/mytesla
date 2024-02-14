01ce9f8b387c7a175533a54dd5b3baf5b832613c
Pieter Van den Abeele <pvdabeel@mac.com>
2024-02-14 19:18:18 +0100

    Disabled compositor images - url changes

--- mytesla.15m.py
+++ mytesla.15m.py
@@ -1158,7 +1158,6 @@ class TeslaVehicle(dict):
             my_headers = {'Accept-Language': 'en-US,en;q=0.5', 'Accept-Encoding': 'gzip, deflate', 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8', 'Upgrade-Insecure-Requests': '1', 'DNT': '1', 'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'} 
             composed_url = self.compose_url(model,size,view,background)
             composed_img = requests.get(composed_url,headers=my_headers)
-        
             if (len(composed_img.content) > 309):
                 with open(state_dir+'/mytesla-composed-'+str(self['vehicle_id'])+'-'+str(size)+'-'+view+'-'+str(background)+'.png','wb') as composed_img_cache:
                     composed_img_cache.write(composed_img.content)
@@ -1624,6 +1623,7 @@ def main(argv):
             for view in _SHOW_CAR_PICTURES_:
                 for background in ['1','2']:
                     for size in ['512','1024','2048','4096']:
+                        #print (view)
                         vehicle.compose_image(vehicle_config['car_type'],view=view,size=size,background=background)
 
         battery_loss_cold = int(charge_state['battery_level']) - int(charge_state['usable_battery_level'])
@@ -2105,9 +2105,9 @@ def main(argv):
 
         print ('%s---' % prefix)
         print ('%sVehicle info| color=%s' % (prefix,color))
-        print ('%s--|image=%s href=%s color=%s' % (prefix, vehicle.compose_image(vehicle_config['car_type'],view=_CAR_DEFAULT_PICTURE_), vehicle.compose_url(vehicle_config['car_type']), color))
-        print ('%s--|image=%s alternate=true href=%s color=%s' % (prefix, vehicle.compose_image(vehicle_config['car_type'],view=_CAR_DEFAULT_PICTURE_2_), vehicle.compose_url(vehicle_config['car_type']), color))
-        print ('%s-----' % prefix)
+        #print ('%s--|image=%s href=%s color=%s' % (prefix, vehicle.compose_image(vehicle_config['car_type'],view=_CAR_DEFAULT_PICTURE_), vehicle.compose_url(vehicle_config['car_type']), color))
+        #print ('%s--|image=%s alternate=true href=%s color=%s' % (prefix, vehicle.compose_image(vehicle_config['car_type'],view=_CAR_DEFAULT_PICTURE_2_), vehicle.compose_url(vehicle_config['car_type']), color))
+        #print ('%s-----' % prefix)
         print ('%s--Name: 			%s | color=%s' % (prefix, vehicle_name, color))
         print ('%s--VIN: 			%s | terminal=true shell="echo %s | pbcopy" color=%s' % (prefix, vehicle_vin, vehicle_vin, color))
         print ('%s--Firmware:		%s | terminal=true shell="echo %s | pbcopy" color=%s' % (prefix, vehicle_state['car_version'],vehicle_state['car_version'], color))
@@ -2139,10 +2139,10 @@ def main(argv):
            except: 
               option_description = 'Unknown'
            print ('%s----%s:\t\t %s | color=%s' % (prefix, option, option_description,info_color))
-        print ('%s--Images| color=%s' % (prefix , color))
-        for view in _SHOW_CAR_PICTURES_:
-           print ('%s----|image=%s href=%s color=%s' % (prefix, vehicle.compose_image(vehicle_config['car_type'],size=512,view=view,background='1'), vehicle.compose_url(vehicle_config['car_type'],size=2048,view=view,background='1'), color))
-           print ('%s----|image=%s alternate=true href=%s color=%s' % (prefix, vehicle.compose_image(vehicle_config['car_type'],size=512,view=view,background='2'), vehicle.compose_url(vehicle_config['car_type'],size=2048,view=view,background='2'), color))
+        #print ('%s--Images| color=%s' % (prefix , color))
+        #for view in _SHOW_CAR_PICTURES_:
+        #   print ('%s----|image=%s href=%s color=%s' % (prefix, vehicle.compose_image(vehicle_config['car_type'],size=512,view=view,background='1'), vehicle.compose_url(vehicle_config['car_type'],size=2048,view=view,background='1'), color))
+        #   print ('%s----|image=%s alternate=true href=%s color=%s' % (prefix, vehicle.compose_image(vehicle_config['car_type'],size=512,view=view,background='2'), vehicle.compose_url(vehicle_config['car_type'],size=2048,view=view,background='2'), color))
 
         print ('%s-----' % prefix)
         print ('%s--Odometer: 		%s %s | color=%s' % (prefix, convert_distance(distance_unit,vehicle_state['odometer']), distance_unit, color))
